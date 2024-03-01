eed89df36a85bdb984793efdb07b61f1de27a29f
muchailouzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-01 21:19:10 +0200

    Create class and Interface for future Google authorization

---
+++ Ryzhanovskyi.University.Tinder.Core/DIConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ryzhanovskyi.University.Tinder.Core.Interfaces;
+using Ryzhanovskiy.University.Tinder.Core.Services;
+using Ryzhanovskiy.University.Tinder.Models.Configuration;
+
+namespace Ryzhanovskiy.University.Tinder.Core
+{
+    public static class DIConfiguration
+    {
+        public static void RegisterCoreDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<IGoogleAuthInterface, GoogleAuthService>();
+        }
+
+        public static void RegisterCoreConfiguration(this IServiceCollection services, IConfigurationRoot configuration)
+        {
+            services.Configure<AppConfig>(configuration.GetSection("AppConfig"));
+        }
+    }
+}
+
---
+++ Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Options;
+using Ryzhanovskiy.University.Tinder.Core.Constants;
+using Ryzhanovskyi.University.Tinder.Core.Interfaces;
+using Ryzhanovskiy.University.Tinder.Models.Configuration;
+
+
+namespace Ryzhanovskiy.University.Tinder.Core.Services
+{
+    public class GoogleAuthService : IGoogleAuthInterface
+    {
+
+    }
+}
