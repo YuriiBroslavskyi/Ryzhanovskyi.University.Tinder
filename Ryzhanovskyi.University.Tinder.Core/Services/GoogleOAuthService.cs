564fe1709d2b529dd36468e505576b8aaaa7a787
muchailouzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-03 13:14:24 +0200

    Created in index file sign in button for future authorization with this button. Added ClientId and ClientSecret constant variables for authorization with google account.

--- Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
+++ Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
@@ -8,6 +8,7 @@ namespace Ryzhanovskiy.University.Tinder.Core.Services
 {
     public class GoogleAuthService : IGoogleAuthInterface
     {
-
+        private const string ClientId = "460055857959-jo5beonip1crjebins8rtbus4796flua.apps.googleusercontent.com";
+        private const string ClientSecret = "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";
     }
 }
