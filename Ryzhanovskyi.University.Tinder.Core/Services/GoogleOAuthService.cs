53260bad3b9e68db0e0b96403645fad78091c9fe
muchailouzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-03 14:01:26 +0200

    Create 3 empty function for generating request url, exchanging code for token and refresh token

--- Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
+++ Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
@@ -1,14 +1,29 @@
 ﻿using Microsoft.Extensions.Options;
 using Ryzhanovskiy.University.Tinder.Core.Constants;
-using Ryzhanovskyi.University.Tinder.Core.Interfaces;
 using Ryzhanovskiy.University.Tinder.Models.Configuration;
+using System.Security.Cryptography.X509Certificates;
 
 
 namespace Ryzhanovskiy.University.Tinder.Core.Services
 {
-    public class GoogleAuthService : IGoogleAuthInterface
+    public class GoogleAuthService
     {
         private const string ClientId = "460055857959-jo5beonip1crjebins8rtbus4796flua.apps.googleusercontent.com";
         private const string ClientSecret = "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";
+
+        public static string GenerateOAuthRequestUrl()
+        {
+
+        }
+
+        public static object ExchangeCodeOnToken(string code)
+        {
+
+        }
+
+        public static object RefreshToken(string refreshToken)
+        {
+
+        }
     }
 }
