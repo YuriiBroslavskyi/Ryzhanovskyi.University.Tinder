c4ed03ce264bd927d47d59d0c2ce90a8e0b603f8
muchailouzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-03 16:04:47 +0200

    Fill the first function which will generate the reques for authorization server

--- Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
+++ Ryzhanovskyi.University.Tinder.Core/Services/GoogleOAuthService.cs
@@ -1,28 +1,44 @@
-﻿using Microsoft.Extensions.Options;
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Options;
 using Ryzhanovskiy.University.Tinder.Core.Constants;
 using Ryzhanovskiy.University.Tinder.Models.Configuration;
+using Ryzhanovskyi.University.Tinder.Core.Interfaces;
+using System.Runtime.Intrinsics.Arm;
 using System.Security.Cryptography.X509Certificates;
 
 
 namespace Ryzhanovskiy.University.Tinder.Core.Services
 {
-    public class GoogleAuthService
+    public class GoogleAuthService : IGoogleAuthInterface
     {
         private const string ClientId = "460055857959-jo5beonip1crjebins8rtbus4796flua.apps.googleusercontent.com";
         private const string ClientSecret = "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";
 
-        public static string GenerateOAuthRequestUrl()
+        public static string GenerateOAuthRequestUrl(string scope, string redirectUrl, string codeChellange)
         {
+            var oAuthServerEndpoint = "https://accounts.google.com/o/oauth2/v2/auth";
 
+            var queryParams = new Dictionary<string, string>
+            {
+                {"client_id", ClientId},
+                {"redirected_uri", redirectUrl},
+                {"response_type", "code"},
+                {"scope", scope },
+                {"code_challenge", codeChellange },
+                {"code_challenge_methode","S256"}
+            };
+            var url = QueryHelpers.AddQueryString(oAuthServerEndpoint, queryParams);
+            return url;
         }
 
         public static object ExchangeCodeOnToken(string code)
         {
-
+            throw new NotImplementedException();
         }
 
         public static object RefreshToken(string refreshToken)
         {
+            throw new NotImplementedException();
 
         }
     }
