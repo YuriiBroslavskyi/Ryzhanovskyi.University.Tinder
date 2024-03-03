8c21e84874ee7004364d707b79d259fbb125f7f0
muchailouzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-03 17:41:44 +0200

    complete making first controller

--- Ryzhanovskyi.University.Tinder.Web/Controllers/GoogleOAuthController.cs
+++ Ryzhanovskyi.University.Tinder.Web/Controllers/GoogleOAuthController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Ryzhanovskiy.University.Tinder.Core.Services;
+using Ryzhanovskyi.University.Tinder.Core.Services;
+using System.Security.Cryptography;
 
 
 namespace Ryzhanovskiy.University.Tinder.API.Controllers
@@ -8,14 +10,19 @@ namespace Ryzhanovskiy.University.Tinder.API.Controllers
     {
         public IActionResult RedirectOnOAuthServer()
         {
-            var url = GoogleAuthService.GenerateOAuthRequestUrl();
+            var scope = "https://www.googleapis.com/auth/userinfo.email\t";
+            var redirectUrl = "/GoogleOAuth/Code";
+
+            var codeVerify = Guid.NewGuid().ToString();
+            var codeChallenge = Sha256code.ComputeHash(codeVerify);
+            var url = GoogleAuthService.GenerateOAuthRequestUrl(scope, redirectUrl, codeChallenge);
             return Redirect(url);
         }
 
         public IActionResult Code(string code)
         {
             GoogleAuthService.ExchangeCodeOnToken(code);
-            return Ok();
+            return Ok();                                                                                                    
         }
     }
 }
