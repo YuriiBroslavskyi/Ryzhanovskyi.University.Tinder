eebf0c0d788761b14b45b09d9d03b26abf2e3ac5
muchailouzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-03 17:02:24 +0200

    I make a separate file with result of post method which we used and will return parametrs that we need

---
+++ Ryzhanovskyi.University.Tinder.Core/Services/TokenResult.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+
+namespace Ryzhanovskiy.University.Tinder.Core.Services
+{
+    public partial class GoogleAuthService
+    {
+        public class TokenResult
+            {
+                [JsonProperty("access_token")]
+                public string AccessToken { get; set; }
+
+                [JsonProperty("expepires_in")]
+                public string ExperisIn { get; set; }
+
+                [JsonProperty("scope")]
+                public string Scope { get; set; }
+
+                [JsonProperty("token_type")]
+                public string TokenType { get; set; }
+           
+                [JsonProperty("refresh_token")]
+                public string RefreshToken { get; set; }
+
+            }
+    }
+
+
+}
