e7004993da106a8dbf9f631194667857394fa481
Михайло Рижановський <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-12 00:10:43 +0200

    Make the sending email functionality, but only in for swager

---
+++ Ryzhanovskyi.University.Tinder.Core/Services/EmailSender.cs
@@ -0,0 +1,34 @@
+﻿using Ryzhanovskyi.University.Tinder.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ryzhanovskyi.University.Tinder.Core.Services
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "gnomelovesender@outlook.com";
+            var pswd = "gnomeLove228!";
+
+            var client = new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, pswd)
+            };
+            return client.SendMailAsync(
+                new MailMessage(from: mail,
+                                to: email,
+                                subject,
+                                message
+                                ));
+    }
+    }
+}
---
+++ Ryzhanovskyi.University.Tinder.Web/Controllers/SendingEmail.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ryzhanovskyi.University.Tinder.Core.Interfaces;
+using System.Threading.Tasks;
+
+namespace Ryzhanovskyi.University.Tinder.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendingEmailController : ControllerBase
+    {
+        private readonly IEmailSender _emailSender;
+
+        public SendingEmailController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendEmailAsync([FromBody] EmailModel model)
+        {
+            try
+            {
+                // Send email asynchronously
+                await _emailSender.SendEmailAsync(model.Email, model.Subject, model.Message);
+                return Ok("Email sent successfully");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while sending the email: {ex.Message}");
+            }
+        }
+    }
+
+    public class EmailModel
+    {
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
