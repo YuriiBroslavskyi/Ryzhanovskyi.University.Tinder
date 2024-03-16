bb5aac745dc843d5fa0ac33b99dd699c8d754005
Михайло Рижановський <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-17 01:33:53 +0200

    correct some html mistakes, create button to login and register account, and create profileCreation.cshtml for setting up profiles.

---
+++ Ryzhanovskyi.University.Tinder.Core/Middleware/AuthMiddleware.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Ryzhanovskyi.University.Tinder.Models.Models;
+using Ryzhanovskyi.University.Tinder.Web.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication;
+
+namespace Ryzhanovskyi.University.Tinder.Core.Middleware
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IServiceProvider _serviceProvider;
+
+        public AuthMiddleware(RequestDelegate next, IServiceProvider serviceProvider)
+        {
+            _next = next;
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+
+                if (!context.Request.Headers.ContainsKey("Authorization"))
+                {
+                    context.Response.Headers["Location"] = "/Auth";
+
+                }
+
+                await _next(context);
+            }
+        }
+    }
+}
---
+++ Ryzhanovskyi.University.Tinder.Web/Pages/Account/ProfileCreation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Ryzhanovskyi.University.Tinder.Web.Pages.Account
+{
+    public class ProfileCreationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
