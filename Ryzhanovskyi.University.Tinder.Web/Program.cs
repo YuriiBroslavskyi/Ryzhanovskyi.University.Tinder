ff29dbc839fc2125e366d9d84c244fbdddc63f58
Михайло Рижановський <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-03-26 15:22:46 +0200

    try

--- Ryzhanovskyi.University.Tinder.Web/Program.cs
+++ Ryzhanovskyi.University.Tinder.Web/Program.cs
@@ -1,14 +1,12 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Ryzhanovskiy.University.Tinder.Core;
 using Ryzhanovskyi.University.Tinder.Core.Interfaces;
 using Ryzhanovskyi.University.Tinder.Core.Services;
 using Ryzhanovskyi.University.Tinder.Models.Models;
 using Ryzhanovskyi.University.Tinder.Web.Data;
-using System.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
@@ -17,61 +15,53 @@ var configuration = builder.Configuration;
 services.AddDbContext<DataContext>(options =>
     options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
-/*services.AddAuthentication(options =>
-{
-    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
-})
-.AddCookie()
-.AddGoogle(googleOptions =>
+
+services.AddAuthentication().AddGoogle(googleOptions =>
 {
     googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
     googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-});*/
+    googleOptions.CallbackPath = "/signin-google/";
+});
 
 services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
         options.LoginPath = "/Auth/Login";
         options.Cookie.Name = "AshProgHelpCookie";
+    });         
 
-    });
+services.AddIdentity<IdentityUser, IdentityRole>(options =>
+    options.SignIn.RequireConfirmedAccount = true)
+.AddEntityFrameworkStores<DataContext>()
+.AddDefaultTokenProviders();
 
-
-services.AddIdentity<User, IdentityRole>(
-    options =>
-    {
-        options.Password.RequiredUniqueChars = 0;
-        options.Password.RequireUppercase = false;
-        options.Password.RequireLowercase = false;
-        options.Password.RequiredLength = 6;
-        options.Password.RequireNonAlphanumeric = false;
-    }).AddEntityFrameworkStores<DataContext>().AddDefaultTokenProviders();
+/*services.AddAuthorization(options =>
+{
+    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+        .RequireAuthenticatedUser()
+        .Build();
+});*/
 
 services.AddScoped<IAuthService, AuthService>();
 services.AddScoped<IProfileService, ProfileService>();
 
 services.AddTransient<IEmailSender, EmailSender>();
-services.AddControllersWithViews();
 
+services.AddControllersWithViews();
 builder.Services.RegisterCoreConfiguration(builder.Configuration);
 builder.Services.RegisterCoreDependencies();
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddRazorPages();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -80,8 +70,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
 app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.UseCookiePolicy();
 
