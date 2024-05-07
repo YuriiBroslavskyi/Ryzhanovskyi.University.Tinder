85ef0396b29db194a6465eb70906ff09b1a136d6
muchailoliluzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-05-07 18:57:39 +0300

    try2

--- TinderClone/settings.py
+++ TinderClone/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 #   ic files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = './static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
