95dc240786ae19d92e0f02ce52ca7dc6007c98c7
muchailoliluzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-05-20 23:48:02 +0300

    try#2323123

--- TinderClone/settings.py
+++ TinderClone/settings.py
@@ -162,8 +162,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
