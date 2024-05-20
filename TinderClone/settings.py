f601e0ebe243edfbac767442b35b99829a0e7078
muchailoliluzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-05-20 22:46:45 +0300

    in settings.py try to on the static and parse the favicon on html pages

--- TinderClone/settings.py
+++ TinderClone/settings.py
@@ -148,9 +148,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "users/static/css",
-]
+STATIC_ROOT = BASE_DIR / "users/static/css"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
