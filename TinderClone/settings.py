f9814335e3e520245199671d40810a89e6c27c51
muchailoliluzivert <mykhailo.ryzhanovskyi@lnu.edu.ua>
2024-05-21 17:11:16 +0300

    WWWW

--- TinderClone/settings.py
+++ TinderClone/settings.py
@@ -107,25 +107,25 @@ WSGI_APPLICATION = 'TinderClone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "mssql",
-#         "NAME": "TinderCloneDb",
-#         "USER": "dmin",
-#         "PASSWORD": "gnomeLove228!",
-#         "HOST": "loveconnect.database.windows.net",
-#         "PORT": "1433",
-#         "OPTIONS": {"driver": "ODBC Driver 18 for SQL Server", 
-#         },
-#     },
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "mssql",
+        "NAME": "TinderCloneDb",
+        "USER": "dmin",
+        "PASSWORD": "gnomeLove228!",
+        "HOST": "loveconnect.database.windows.net",
+        "PORT": "1433",
+        "OPTIONS": {"driver": "ODBC Driver 18 for SQL Server", 
+        },
+    },
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
